6e09ec368304b77018583660c538d94c5a37daed
shizhiqian <18510422820@163.com>
2020-04-11 14:38:57 +0800

    适配器模式：以 Interator 接口为适配器接口，Enumeration 为被适配接口，以适配 Enumeration接口方法的调用

---
+++ src/main/java/com/adapter/EnumerationInterator.java
@@ -0,0 +1,36 @@
+package com.adapter;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+/**
+ * @ClassName EnumerationInterator
+ * @Description
+ * @Author ShiZhiQian
+ * @Date 2020/4/11 14:26
+ **/
+public class EnumerationInterator implements Iterator {
+
+    Enumeration enumeration;
+
+
+    public EnumerationInterator(Enumeration enumeration) {
+        this.enumeration = enumeration;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        return enumeration.hasMoreElements();
+    }
+
+    @Override
+    public Object next() {
+        return enumeration.nextElement();
+    }
+}
